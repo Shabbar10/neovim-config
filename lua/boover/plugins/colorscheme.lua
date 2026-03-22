c4e29ca5c72fcfc7373233023e1abc2fc392c94a
shabbar-os <shabbar.adamjee@onlinesales.ai>
2026-03-22 17:48:30 +0530

    colorscheme fix

--- lua/boover/plugins/colorscheme.lua
+++ lua/boover/plugins/colorscheme.lua
@@ -10,6 +10,10 @@ return {
 				dark = "mocha",
 			},
 			transparent_background = false, -- disables setting the background color.
+			float = {
+				transparent = false, -- enable transparent floating windows
+				solid = false, -- use solid styling for floating windows, see |winborder|
+			},
 			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
 			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
 			dim_inactive = {
@@ -35,14 +39,33 @@ return {
 				operators = {},
 				-- miscs = {}, -- Uncomment to turn off hard-coded styles
 			},
+			lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+					ok = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+					ok = { "underline" },
+				},
+				inlay_hints = {
+					background = true,
+				},
+			},
 			color_overrides = {},
 			custom_highlights = {},
 			default_integrations = true,
+			auto_integrations = false,
 			integrations = {
 				cmp = true,
 				gitsigns = true,
 				nvimtree = true,
-				treesitter = true,
 				notify = false,
 				mini = {
 					enabled = true,
@@ -53,6 +76,6 @@ return {
 		})
 
 		-- setup must be called before loading
-		vim.cmd.colorscheme("catppuccin")
+		vim.cmd.colorscheme("catppuccin-nvim")
 	end,
 }
